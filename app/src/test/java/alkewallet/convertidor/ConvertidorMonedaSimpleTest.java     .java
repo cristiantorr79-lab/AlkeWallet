ffb931f4a11919f0f5ca4d37f1d401baa07fd57f
cristiantorr79-lab <cristiantorr79@gmail.com>
2026-02-13 17:34:19 -0300

    AlkeWallet V3.2

--- app/src/test/java/alkewallet/convertidor/ConvertidorMonedaSimpleTest.java     .java
+++ app/src/test/java/alkewallet/convertidor/ConvertidorMonedaSimpleTest.java     .java
@@ -5,9 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.google.common.annotations.VisibleForTesting;
-
-public class CnvertidorMonedaSimpleTest {
+public class ConvertidorMonedaSimpleTest {
 
     private ConvertidorMoneda convertidor;
 
@@ -20,6 +18,7 @@ public class CnvertidorMonedaSimpleTest {
     void convertirCLPaUSD() {
         // Arrange
         double monto = 850000; // 850,000 CLP
+
         // Act
         double resultado = convertidor.convertir(monto, "CLP", "USD");
 
@@ -30,9 +29,9 @@ public class CnvertidorMonedaSimpleTest {
 
     @Test
     void convertirUSDaCLP() {
-
         // Arrange
         double monto = 100; // 100 USD
+
         // Act
         double resultado = convertidor.convertir(monto, "USD", "CLP");
 
@@ -45,6 +44,7 @@ public class CnvertidorMonedaSimpleTest {
     void convertirCLPaEUR() {
         // Arrange
         double monto = 900000; // 900,000 CLP
+
         // Act
         double resultado = convertidor.convertir(monto, "CLP", "EUR");
 
